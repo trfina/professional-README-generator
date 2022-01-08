64829691908ca71355a3ebf6d444f33f4f62f265
Tammy Fina <finatammy@gmail.com>
2022-01-08 18:04:30 -0500

    markdoun fields work - formating readme next

--- README.md
+++ README.md
@@ -1,84 +1,52 @@
-# Professional README.md Generator
 
-## Description 
-This project dynamically generates a professional README.md file using the Inquirer package.
+# fdsjkl
+gpl-2.0.en.html
+GPL_v2-blue
 
+## Description
+fdsjk
 The link to the deployed application is here:
 
-[Professional README.md Generator](https://github.com/trfina/professional-README-generator).
+[Repository for this Project](dfskj)
+
+Example of finished product:
+![Finished Mock Up](./assets/images/testscreenshot.png)
 
 ## Table of Contents
-
 * [Installation](#installation)
 * [Usage](#usage)
 * [Contributing](#contributing)
+* [Tests](#tests)
 * [Credits](#credits)
 * [License](#license)
-
+* [Questions](#questions)
 
 ## Installation
+sfdkj
 
-Node.js must be installed on your machine.
+## Usage
+sfdjkl
 
-Inquirer package must be installed in the directory where your files are:
-(https://npmjs.com/package/inquirer)
-
-## Usage 
-
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-```md
-![alt text](assets/images/screenshot.png)
-```
-<!-- SCREENSHOTS to ADD:
-how to start program
-input screen
-final generated README.md file -->
-
+## Contributing
+sfdf
+    
+## Tests
+undefined
+    
 ## Credits
-
-Starter Code for Professional README.md:
-(https://github.com/coding-boot-camp/potential-enigma)
-
-Tutorials:
-https://github.com/luong-komorebi/Markdown-Tutorial/blob/master/README.md
-
+undefined
+    
 ## License
-
-MIT License
-
-Copyright (c) [2021] [Tammy Fina]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
----
-
-🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
+GNU GPL v2
+    
 ## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
 
 ## Features
 
-If your project has a lot of features, consider adding a heading called "Features" and listing them there.
+## Questions
 
+GitHub Username:
+sdflkj
+Additional Questions?  Contact me at:
+sfdjkl
 
-## Contributing
-
-The [Contributor Covenant](https://www.contributor-covenant.org/) 
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
--- index.js
+++ index.js
@@ -1,13 +1,210 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { listenerCount } = require('process');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => 
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address (Required)',
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log('Please enter your email address!');
+          return false;
+        }
+      }
+    },
+    
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What it the title of your project?',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter the title of your project!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'desc',
+      message: 'Enter the description of your project?',
+      validate: descInput => {
+        if (descInput) {
+          return true;
+        } else {
+          console.log('Please enter the description of your project!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'repos',
+      message: 'Enter the GitHub repository of your project.',
+      validate: reposInput => {
+        if (reposInput) {
+          return true;
+        } else {
+          console.log('Please enter GitHub repository of your project!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'Enter installation instructions for your project.'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use of your project.'
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'Enter how to contribute to your project.'
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Provide examples on how to run tests for your application, if any.'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Select a license for your project,if any.',
+      choices: ['MIT','Apache 2.0','GNU GPL v2', 'Other']
+    }          
+  ]);
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+function generateREADME(data) {
+  var licenseCode = '';
+  var licenseColor = '';
+  
+  console.log(data.license);
+
+  if ((data.license) === 'MIT') { 
+      licenseCode = 'MIT';
+      licenseColor = 'MIT-yellow';
+    }
+    else if ((data.license) === 'Apache 2.0') {
+      licenseCode = 'Apache-2.0';
+      licenseColor = 'Apache-2.0-blue';
+    }
+    else if ((data.license) === 'GNU GPL v2') {
+      licenseCode = 'gpl-2.0.en.html';
+      licenseColor = 'GPL_v2-blue';
+    }
+    else {
+      return;
+    };   
+
+  console.log(licenseCode, licenseColor);
+
+  // console.log(data.name);
+  // console.log(data.license);
+  // console.log(data);
+  
+
+  return`
+# ${data.title}
+${licenseCode}
+${licenseColor}
+
+## Description
+${data.desc}
+The link to the deployed application is here:
+
+[Repository for this Project](${data.repos})
+
+Example of finished product:
+![Finished Mock Up](./assets/images/testscreenshot.png)
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribute}
+    
+## Tests
+${data.tests}
+    
+## Credits
+${data.credit}
+    
+## License
+${data.license}
+    
+## Badges
+
+## Features
+
+## Questions
+
+GitHub Username:
+${data.github}
+Additional Questions?  Contact me at:
+${data.email}
+
+`
+};
+
+promptUser()
+  .then((data) => { 
+    
+    fs.writeFile('./README.md', generateREADME(data), err => {
+      if (err) throw new Error(err);
+
+      console.log('Page Created!  Check out README.md in this directory.');
+    });
+  });
+  
